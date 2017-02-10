4c2c0f64e6ac450a5245fb49321511f278200e1e
Marat Nuriev <nurieff@gmail.com>
2017-02-10 18:28:47 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,190 @@
+var
+    SpritusList = require('./src/list')
+    , SpritusCssReplacer = require('./src/css-replacer')
+    , imagemin = require('imagemin')
+    , through = require('through2').obj
+    , mkdirp = require('mkdirp')
+    , fs = require('fs')
+    , prettyBytes = require('pretty-bytes')
+    ;
+
+function Spritus(config) {
+
+    this.config = {
+        padding: 2,
+        algorithm: 'top-down', // left-right,diagonal,alt-diagonal,binary-tree
+        saveImage: false,
+        withImagemin: false,
+        withImageminPlugins: null,
+        imageDirCSS: '../images/',
+        imageDirSave: 'public/images/'
+    };
+
+    /**
+     * @type {Buffer}
+     */
+    this.css = null;
+
+    /**
+     * @type {String}
+     */
+    this.strCSS = null;
+
+    this.imgStream = through();
+    this.cssStream = through();
+    this.retStream = null;
+
+    /**
+     * @type {Function}
+     */
+    this.endCallback = null;
+
+    /**
+     * @type {Array}
+     */
+    this.imgFiles = [];
+
+    /**
+     * @type {SpritusList}
+     */
+    this.SpritusList = null;
+
+    this.rootPath = process.cwd() + '/';
+
+    if (config) {
+        for (var key in config) {
+            if (!config.hasOwnProperty(key)) continue;
+
+            if (key in this.config) {
+                this.config[key] = config[key];
+            }
+        }
+    }
+}
+
+Spritus.prototype.onData = function (file, encoding, cb) {
+    this.css = file;
+    cb();
+};
+
+Spritus.prototype.onEnd = function (cb) {
+
+    if (!this.css) {
+        this.imgStream.push(null);
+        this.cssStream.push(null);
+        return cb();
+    }
+
+    this.endCallback = cb;
+
+    this.strCSS = this.css.contents.toString();
+    this.SpritusList = new SpritusList(this);
+    var self = this;
+
+    this.strCSS.replace(/spritus\-[^\(]+\(\"([^\"]+)\"/ig, function (str) {
+        self.SpritusList.push(arguments[1]);
+        return str;
+    });
+
+    this.SpritusList.run(this.runHandler.bind(this));
+};
+
+Spritus.prototype._saveFile = function (file, path, fromImagemin) {
+    var filepath = path + file.path;
+
+    fs.unlink(filepath, function (err) {
+        if (err) {
+
+        }
+        fs.writeFileSync(filepath, file.contents);
+
+        if (!fromImagemin) {
+            console.log('spritus[save file]: ' + path + file.path);
+        }
+    });
+};
+
+Spritus.prototype._saveImagemin = function (file, path) {
+
+    var self = this;
+
+    imagemin.buffer(file.contents, {
+        plugins: this.config.withImageminPlugins ? this.config.withImageminPlugins : []
+    })
+        .then(function (data) {
+
+            var originalSize = file.contents.length;
+            var optimizedSize = data.length;
+            var saved = originalSize - optimizedSize;
+            var percent = (originalSize > 0 ? (saved / originalSize) * 100 : 0).toFixed(1).replace(/\.0$/, '');
+            var msg = saved > 0 ? '- saved ' + prettyBytes(saved) + ' (' + percent + '%)' : ' -';
+            console.log('spritus[imagemin]: ' + path + file.path + ' ' + msg);
+
+            file.contents = data;
+
+            self._saveFile(file, path, true);
+        })
+        .catch(function (err) {
+            console.log('imagemin: ' + file.path + ' Error');
+            console.log(err);
+        });
+};
+
+Spritus.prototype.runHandler = function (imgFile) {
+
+    this.imgFiles.push(imgFile);
+
+    if (!this.SpritusList.isComplete()) return;
+
+    this.strCSS = SpritusCssReplacer.makeCSS(this.strCSS, this.SpritusList);
+
+    var i, l, path;
+
+    if (!this.config.saveImage) {
+        for (i = 0, l = this.imgFiles.length; i < l; ++i) {
+            this.imgStream.push(this.imgFiles[i]);
+        }
+    } else {
+        path = this.config.imageDirSave.indexOf('/') === 0 ? this.config.imageDirSave : this.rootPath + this.config.imageDirSave;
+        var self = this;
+        mkdirp(path, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            var i, l;
+            if (!self.config.withImagemin) {
+                for (i = 0, l = self.imgFiles.length; i < l; ++i) {
+                    self._saveFile(self.imgFiles[i], path);
+                }
+            } else {
+                for (i = 0, l = self.imgFiles.length; i < l; ++i) {
+                    self._saveImagemin(self.imgFiles[i], path);
+                }
+            }
+
+        });
+
+
+    }
+
+    this.css.contents = new Buffer(this.strCSS);
+
+    this.imgStream.push(null);
+    this.cssStream.push(this.css);
+
+    this.retStream.push(this.css);
+    this.endCallback();
+};
+
+module.exports = function (config) {
+
+    var S = new Spritus(config);
+
+    S.retStream = through(S.onData.bind(S), S.onEnd.bind(S));
+    S.retStream.css = S.cssStream;
+    S.retStream.img = S.imgStream;
+
+    return S.retStream;
+};
---
+++ src/css-replacer.js
@@ -0,0 +1,156 @@
+/**
+ * @param {String} css
+ * @param {SpritusList} SpritusList
+ * @constructor
+ */
+function SpritusCssReplacer(css, SpritusList) {
+	/**
+	 * @type {String}
+	 */
+	this.css = css;
+
+	/**
+	 * @type {SpritusList}
+	 */
+	this.SpritusList = SpritusList;
+}
+
+SpritusCssReplacer.prototype._reg = function(mod,dopArgs) {
+
+	var r = [];
+	r.push('spritus');
+	if (mod) {
+		r.push("\\-" + mod);
+	}
+
+	if (dopArgs) {
+		r.push("\\(\\\"([^\\)\\\"]+)\\\"");
+		r.push(",?\\s*?\\\"?([^\\)\\\"]*)\\\"?\\)");
+	} else {
+		r.push("\\(\\\"([^\\)\\\"]+)\\\"\\)");
+	}
+
+	return new RegExp(r.join(''),'ig');
+};
+
+SpritusCssReplacer.prototype._position = function() {
+	var self = this;
+	this.css = this.css.replace(this._reg('position',true), function() {
+		var str = arguments[1];
+		var img = arguments[2];
+
+		if (!img || !self.SpritusList.get(str)) {
+			return '0px 0px';
+		}
+
+		return self.SpritusList.get(str).position(img);
+	});
+
+	return this;
+};
+
+SpritusCssReplacer.prototype._url = function() {
+	var self = this;
+	this.css = this.css.replace(this._reg('url'), function() {
+		var str = arguments[1];
+
+		if (!self.SpritusList.get(str)) {
+			return '';
+		}
+
+		return self.SpritusList.get(str).url();
+	});
+
+	return this;
+};
+
+SpritusCssReplacer.prototype._height = function() {
+	var self = this;
+	this.css = this.css.replace(this._reg('height',true), function() {
+
+		var str = arguments[1];
+		var img = arguments[2];
+
+		if (!img) {
+			return self.SpritusList.get(str).height();
+		}
+
+		if (!self.SpritusList.get(str)) {
+			return 'auto';
+		}
+
+		return self.SpritusList.get(str).height(img);
+	});
+
+	return this;
+};
+
+SpritusCssReplacer.prototype._width = function() {
+	var self = this;
+	this.css = this.css.replace(this._reg('width',true), function() {
+
+		var str = arguments[1];
+		var img = arguments[2];
+
+		if (!img) {
+			return self.SpritusList.get(str).width();
+		}
+
+		if (!self.SpritusList.get(str)) {
+			return 'auto';
+		}
+
+		return self.SpritusList.get(str).width(img);
+	});
+
+	return this;
+};
+
+SpritusCssReplacer.prototype._size = function() {
+	var self = this;
+	this.css = this.css.replace(this._reg('size'), function() {
+
+		var str = arguments[1];
+
+		if (!self.SpritusList.get(str)) {
+			return 'auto auto';
+		}
+
+		return self.SpritusList.get(str).size();
+	});
+
+	return this;
+};
+
+/**
+ * @returns {SpritusCssReplacer}
+ */
+SpritusCssReplacer.prototype.run = function() {
+	this._url()
+		._position()
+		._height()
+		._width()
+		._size()
+	;
+
+	return this;
+};
+
+/**
+ * @returns {String}
+ */
+SpritusCssReplacer.prototype.getCss = function() {
+	return this.css;
+};
+
+/**
+ * @param {String} css
+ * @param {SpritusList} SpritusList
+ * @returns {String}
+ */
+SpritusCssReplacer.makeCSS = function(css, SpritusList) {
+	var R = new SpritusCssReplacer(css, SpritusList);
+	return R.run().getCss();
+};
+
+module.exports = SpritusCssReplacer;
