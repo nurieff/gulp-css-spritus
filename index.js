1433ff88e03ed63b17d00dc60e0a42c60135ab64
Marat Nuriev <nurieff@gmail.com>
2017-06-03 00:42:03 +0500

    nodejs 8 oops

--- index.js
+++ index.js
@@ -117,14 +117,16 @@ Spritus.prototype._saveFile = function (file, path, fromImagemin) {
   var filepath = path + file.path;
 
   fs.unlink(filepath, function (err) {
-    if (err) {
+    if (err) throw err;
 
-    }
-    fs.writeFileSync(filepath, file.contents);
+    fs.writeFile(filepath, file.contents, function (err) {
+      if (err) throw err;
 
-    if (!fromImagemin) {
-      console.log('spritus[save file]: ' + path + file.path);
-    }
+      if (!fromImagemin) {
+        console.log('spritus[save file]: ' + path + file.path);
+      }
+    });
+
   });
 };
 
